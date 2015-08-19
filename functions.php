aecc497626da0af7c4aec4d1e4b0677a5fb69475
Ed Adams <edadams@users.noreply.github.com>
2015-08-19 01:06:45 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
  * by Ed Adams: custom CSS for the WP-Admin area for non-administrator users.
  * This should be expanded with some CSS to apply when viewing pages with the WordPress top bar in the future, as some functions hidden here will be visible there.
  */
-function excellence_non_admin_custom_css() {
+function ea_css_for_admin() {
   if( !current_user_can( 'activate_plugins' )) {
     echo '<style type="text/css" id="excellence-editor-custom-css" media="screen">
       #wp-admin-bar-wp-logo, #menu-posts, #menu-comments, #menu-posts-feedback, #menu-tools, #menu-settings, #toplevel_page_jetpack, #collapse-menu, #wp-admin-bar-comments, #wp-admin-bar-notes, #wpfooter, #profile-page h3:nth-of-type(1), #profile-page .form-table:nth-of-type(-n+2), #post-by-email {
@@ -11,22 +11,22 @@ function excellence_non_admin_custom_css() {
     </style>';	
   }
 }
-add_action( 'admin_head', 'excellence_non_admin_custom_css' );
+add_action( 'admin_head', 'ea_css_for_admin' );
 
 
 
 /*
- * by @Ed Adams: custom Dashboard box.
+ * by Ed Adams: custom Dashboard box.
  * Second parameter in wp_add_dashboard_widget is the title, third parameter is the function call which echos the content. HTML safe.
  */
-function excellence_add_dashboard_boxes() {
+function ea_add_dashboard_boxes() {
   global $wp_meta_boxes;
-  wp_add_dashboard_widget( 'custom_help_widget', 'Excellence Aviation on WordPress', 'excellence_dashboard_help_box' );
+  wp_add_dashboard_widget( 'custom_help_widget', 'You are on WordPress!', 'ea_dashboard_box_1' );
 }
-function excellence_dashboard_help_box() {
-  echo '<p>Welcome to Excellence Aviation\'s new webite running on WordPress!</p><p><a class="button button-primary" href="https://docs.google.com/document/d/1217khhFY3o2lyIz8AwI1-uIR7OB7VFau9BGeSPSh18U/edit?usp=sharing">Read the <em>How to use WordPress</em> guide</a></p>';
+function ea_dashboard_box_1() {
+  echo '<p>Welcome to the new WordPress site!</p><p><a class="button button-primary" href="#0">A useful button</em></a></p>';
 }
-add_action( 'wp_dashboard_setup', 'excellence_add_dashboard_boxes' );
+add_action( 'wp_dashboard_setup', 'ea_add_dashboard_boxes' );
 
 
 
@@ -34,9 +34,9 @@ add_action( 'wp_dashboard_setup', 'excellence_add_dashboard_boxes' );
  * by Ed Adams: set the defaults for user-uploaded images to display at full sizes and use sensible defaults.
  * Sensible defaults when uploading media, as I don't want editors to have to always define that images shouldn't display as thumbnails.
  */
-function default_attachment_display_settings() {
+function ea_default_attachment_settings() {
   update_option( 'image_default_align', 'none' );
   update_option( 'image_default_link_type', 'none' );
   update_option( 'image_default_size', 'full' );
 }
-add_action( 'after_setup_theme', 'default_attachment_display_settings' );
+add_action( 'after_setup_theme', 'ea_default_attachment_settings' );
